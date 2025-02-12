aaee60d9996edb31764406db10d5ccf68a5a88de
Vrinda-0703 <vrinda0321.becse24@chitkara.edu.in>
2025-02-12 12:57:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,24 +5,34 @@ int main(){
     scanf("%d", &a);
     scanf("%d", &b);
     scanf("%c",&c);
-    switch (c){
+    switch (c)'
+    '
     case '+':
+    {
         int s=a+b;
         printf("%d",s);
         break;
+    }
     case '-':
+    {
         int m=a-b;
         printf("%d",m);
         break;
+    }
     case '*':
+    {
         int y=a*b;
         printf("%d",y);
         break;
+    }
     case '/':
+    {
         int z=a/b;
         printf("%d",z);
         break;
+    }
     default:
         printf("Error");
+    }
     return 0;}
 }
