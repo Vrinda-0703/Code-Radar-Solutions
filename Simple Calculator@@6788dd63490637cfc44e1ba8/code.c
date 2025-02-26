c90652cb9bcc7cf2298886e71255c97400894e1b
Vrinda-0703 <vrinda0321.becse24@chitkara.edu.in>
2025-02-27 01:17:17 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -31,7 +31,7 @@ int main(){
         printf("%d",z);
         }
         else{
-            printf("Division not possible")
+            printf("Division not possible");
         }
         break;
     }
