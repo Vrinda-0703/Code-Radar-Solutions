8305b7142d02fdecdc0c35ca4091143e43e03f6d
Vrinda-0703 <vrinda0321.becse24@chitkara.edu.in>
2025-03-30 14:11:29 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fibonacciSeries(int n);
+int main(){
+    scanf("%d", &n);
+    int fibonacciSeries();
+    return 0;
+}
+fibonacciSeries(int n){
+    int a,n,t1=0,t2=1,t3;
+    printf("Fibonacci Series: ");
+    for(int i=1;i<=n;i++){
+        printf("%d",t1);
+        t3=t2+t1;
+        t1=t2;
+        t2=t3;
+    }
+    printf("\n");
+}
